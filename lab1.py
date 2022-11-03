3e83de4d4be2bcbf0733cf542890189917bfe113
Arch4ngel21 <adammichal657@gmail.com>
2022-11-03 21:46:25 +0100

    Comments for clarification of the code.

--- lab1.py
+++ lab1.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 
+# Function for dividing square matrix into 4 quarters
 def quarters(X: np.ndarray):
     width, height = X.shape
     half_width, half_height = width // 2, height // 2
@@ -26,6 +27,7 @@ def strassen(A: np.ndarray, B: np.ndarray):
     elif B.size == 1:
         return B[0][0] * A
 
+    # Fancy check for multiple of 2 in matrix shapes
     if (
         A.shape != B.shape
         or bin(A.shape[0]).count("1") != 1
@@ -33,18 +35,22 @@ def strassen(A: np.ndarray, B: np.ndarray):
         or bin(B.shape[0]).count("1") != 1
         or bin(B.shape[1]).count("1") != 1
     ):
+        # new shape values are multiples of 2, the closest to maximum values of A and B shapes,
+        # but higher than them or equal
         new_shape = (
             find_closest_multiple_of_2(np.max([A.shape[0], B.shape[0]])),
             find_closest_multiple_of_2(np.max([A.shape[1], B.shape[1]])),
         )
 
+        # casting A and B to matrices of 0 with a new dimensions
         A_zeros = np.zeros(shape=new_shape)
         B_zeros = np.zeros(shape=new_shape)
         A_zeros[: A.shape[0], : A.shape[1]] = A
-        B_zeros[: B.shape[0] :, : B.shape[1]] = B
+        B_zeros[: B.shape[0], : B.shape[1]] = B
         A = A_zeros
         B = B_zeros
 
+    # division to 4 quarters
     a, b, c, d = quarters(A)
     e, f, g, h = quarters(B)
 
@@ -56,6 +62,7 @@ def strassen(A: np.ndarray, B: np.ndarray):
     p6 = strassen(b - d, g + h)
     p7 = strassen(a - c, e + f)
 
+    # calculating the 4 quarters of the result matrix
     quarter_lu = p5 + p4 - p2 + p6
     quarter_ru = p1 + p2
     quarter_ld = p3 + p4
